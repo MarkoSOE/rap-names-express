60392583a9cfede4887dd30e4205c220adf87348
Marko <markoalhamadani@gmail.com>
2022-06-20 19:24:44 -0400

    .

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 
 let db,
     dbConnectionStr = process.env.DB_STRING,
-    dbName = 'rap'
+    dbName = 'todolist'
 
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
@@ -21,50 +21,50 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 
-app.get('/',(request, response)=>{
-    db.collection('rappers').find().sort({likes: -1}).toArray()
-    .then(data => {
-        response.render('index.ejs', { info: data })
-    })
-    .catch(error => console.error(error))
-})
+// app.get('/',(request, response)=>{
+//     db.collection('rappers').find().sort({likes: -1}).toArray()
+//     .then(data => {
+//         response.render('index.ejs', { info: data })
+//     })
+//     .catch(error => console.error(error))
+// })
 
-app.post('/addRapper', (request, response) => {
-    db.collection('rappers').insertOne({stageName: request.body.stageName,
-    birthName: request.body.birthName, likes: 0})
-    .then(result => {
-        console.log('Rapper Added')
-        response.redirect('/')
-    })
-    .catch(error => console.error(error))
-})
+// app.post('/addRapper', (request, response) => {
+//     db.collection('rappers').insertOne({stageName: request.body.stageName,
+//     birthName: request.body.birthName, likes: 0})
+//     .then(result => {
+//         console.log('Rapper Added')
+//         response.redirect('/')
+//     })
+//     .catch(error => console.error(error))
+// })
 
-app.put('/addOneLike', (request, response) => {
-    db.collection('rappers').updateOne({stageName: request.body.stageNameS, birthName: request.body.birthNameS,likes: request.body.likesS},{
-        $set: {
-            likes:request.body.likesS + 1
-          }
-    },{
-        sort: {_id: -1},
-        upsert: true
-    })
-    .then(result => {
-        console.log('Added One Like')
-        response.json('Like Added')
-    })
-    .catch(error => console.error(error))
+// app.put('/addOneLike', (request, response) => {
+//     db.collection('rappers').updateOne({stageName: request.body.stageNameS, birthName: request.body.birthNameS,likes: request.body.likesS},{
+//         $set: {
+//             likes:request.body.likesS + 1
+//           }
+//     },{
+//         sort: {_id: -1},
+//         upsert: true
+//     })
+//     .then(result => {
+//         console.log('Added One Like')
+//         response.json('Like Added')
+//     })
+//     .catch(error => console.error(error))
 
-})
+// })
 
-app.delete('/deleteRapper', (request, response) => {
-    db.collection('rappers').deleteOne({stageName: request.body.stageNameS})
-    .then(result => {
-        console.log('Rapper Deleted')
-        response.json('Rapper Deleted')
-    })
-    .catch(error => console.error(error))
+// app.delete('/deleteRapper', (request, response) => {
+//     db.collection('rappers').deleteOne({stageName: request.body.stageNameS})
+//     .then(result => {
+//         console.log('Rapper Deleted')
+//         response.json('Rapper Deleted')
+//     })
+//     .catch(error => console.error(error))
 
-})
+// })
 
 app.listen(process.env.PORT || PORT, ()=>{
     console.log(`Server running on port ${PORT}`)
